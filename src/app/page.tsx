23366f43af9bcfd397f5d2c85c9d8eb3822407aa
khai732003 <khaithinhvui2003@gmail.com>
2024-08-02 18:41:46 +0700

    first-cm

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Banner from "@/components/banner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Banner />
+      <div className="container mx-auto px-4 md:px-6 lg:px-8">Homepage nè</div>
+    </main>
+  );
+}
---
+++ src/constants/router.ts
@@ -0,0 +1,18 @@
+export const headerPaths = [
+  {
+    link: "/",
+    label: "HOMEPAGE",
+  },
+  {
+    link: "/service",
+    label: "SERVICES",
+  },
+  {
+    link: "/about",
+    label: "ABOUT",
+  },
+  {
+    link: "/contact",
+    label: "CONTACTS",
+  },
+];
